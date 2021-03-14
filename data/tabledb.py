cbd100b52bb4d9098f5f35ce3a8e47b74bada269
Johan <johan.27011993@gmail.com>
2021-03-14 17:34:22 -0400

    resize changes in dataset

--- data/tabledb.py
+++ data/tabledb.py
@@ -10,7 +10,6 @@ from utils.mypath import MyPath
 from torchvision import transforms as tf
 from glob import glob
 import pandas as pd
-import cv2
 from detectron2.data import transforms as T_
 
 main_dir = '/scratch/b/bkantarc/jfern090/Projects/Lytica/'
@@ -31,18 +30,33 @@ class TableDB(data.Dataset):
         self.csv_file = pd.read_csv(file_name)
         self.transform = transform 
         self.split = split
-        self.resize = T_.ResizeShortestEdge([912, 912], 1600)
-    
+        self.shortest_size = 912
+        self.largest_size = 1600
+
     def __len__(self):
         return len(self.csv_file)
 
     def __getitem__(self, index):
         row = self.csv_file.iloc[index]
         path, target, class_name = row[1], row[2], row[3]
-        #Change this later : hacky way by Johan
-        img = cv2.imread(path)
-        img = self.resize(img)
-        img = Image.fromarray(img)
+        img = Image.open(path)
+        width, height = img.size
+        scale = self.shortest_size * (1.0 / min(width, height))
+
+        if width < height:
+            width, height = self.shortest_size, height * scale
+        else:
+            width, height = width * scale, self.shortest_size
+
+        if max(width, height) > self.largest_size:
+            scale = self.largest_size * 1.0 / max(width, height)
+            height = height * scale
+            width = width * scale
+
+        width = int(width + 0.5)
+        height = int(height + 0.5)
+
+        img = img.resize((width, height))
         im_size = img.size
 
         if self.transform is not None:
