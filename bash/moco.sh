ffb9295678762c877078c626f0e336118225d888
Johan <johan.27011993@gmail.com>
2021-03-14 17:49:45 -0400

    prepare bash

--- bash/moco.sh
+++ bash/moco.sh
@@ -3,13 +3,13 @@
 #SBATCH --gpus-per-node=1
 #SBATCH --time=1:45:00
 #SBATCH --job-name moco
-#SBATCH --bash_output=../bash_output/moco.txt
+#SBATCH --output=bash_output/moco.txt
 #SBATCH -A soscip-3-040
 #SBATCH --mail-type=FAIL
 
 module load anaconda3 cuda cudnn gcc/8.3.0
 source activate detectionEnv
 
-python ../moco_upd.py
+python ../python moco_upd.py --config_env configs/env.yml --config_exp configs/pretext/moco_tabledb.yml
 
 source deactivate
