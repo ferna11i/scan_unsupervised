2cf91ef1e984f346ab94ec974a647b685eeb0114
Johan <johan.27011993@gmail.com>
2021-03-15 04:07:57 -0400

    testing simclr

--- bash/moco.sh
+++ bash/moco.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --gpus-per-node=4
+#SBATCH --gpus-per-node=1
 #SBATCH --time=1:45:00
 #SBATCH --job-name moco
 #SBATCH --output=moco.txt
-#SBATCH -p compute_full_node
 #SBATCH -A soscip-3-040
 #SBATCH --mail-type=FAIL
 
 module load anaconda3
 source activate clusterEnv
 
-python ../moco_upd.py --config_env ../configs/env.yml --config_exp ../configs/pretext/moco_tabledb.yml
+python ../simclr_upd.py --config_env ../configs/env.yml --config_exp ../configs/pretext/moco_tabledb.yml
 
 source deactivate
