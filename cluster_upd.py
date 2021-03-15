7826b9ce40e342082f517278f222563c5057bad3
Johan <johan.27011993@gmail.com>
2021-03-15 19:11:26 -0400

    evaluating moco

--- cluster_upd.py
+++ cluster_upd.py
@@ -7,6 +7,7 @@ import os
 import torch
 import torch.nn.functional as F
 import numpy as np
+from tqdm import tqdm
 
 from utils.config import create_config
 from utils.common_config import get_criterion, get_model, get_train_dataset, \
@@ -29,6 +30,46 @@ parser.add_argument('--config_env',
 parser.add_argument('--config_exp',
                     help='Config file for the experiment')
 args = parser.parse_args()
+
+
+# knn monitor as in InstDisc https://arxiv.org/abs/1805.01978
+# implementation follows http://github.com/zhirongw/lemniscate.pytorch and https://github.com/leftthomas/SimCLR
+def knn_predict(feature, feature_bank, feature_labels, classes, knn_k, knn_t):
+    # # compute cos similarity between each feature vector and feature bank ---> [B, N]
+    # sim_matrix = torch.mm(feature, feature_bank)
+    # # [B, K]
+    # sim_weight, sim_indices = sim_matrix.topk(k=knn_k, dim=-1)
+    # # [B, K]
+    # sim_labels = torch.gather(feature_labels.expand(feature.size(0), -1), dim=-1, index=sim_indices)
+    # sim_weight = (sim_weight / knn_t).exp()
+    #
+    # # counts for each class
+    # one_hot_label = torch.zeros(feature.size(0) * knn_k, classes, device=sim_labels.device)
+    # # [B*K, C]
+    # one_hot_label = one_hot_label.scatter(dim=-1, index=sim_labels.view(-1, 1), value=1.0)
+    # # weighted score ---> [B, C]
+    # pred_scores = torch.sum(one_hot_label.view(feature.size(0), -1, classes) * sim_weight.unsqueeze(dim=-1), dim=1)
+    #
+    # pred_labels = pred_scores.argsort(dim=-1, descending=True)
+    # return pred_labels
+
+    # compute cos similarity between each feature vector and feature bank ---> [B, N]
+    sim_matrix = torch.mm(feature, feature_bank)
+    # [B, K]
+    sim_weight, sim_indices = sim_matrix.topk(k=knn_k, dim=-1)
+    # # [B, K]
+    # sim_labels = torch.gather(feature_labels.expand(feature.size(0), -1), dim=-1, index=sim_indices)
+    # sim_weight = (sim_weight / knn_t).exp()
+    #
+    # # counts for each class
+    # one_hot_label = torch.zeros(feature.size(0) * knn_k, classes, device=sim_labels.device)
+    # # [B*K, C]
+    # one_hot_label = one_hot_label.scatter(dim=-1, index=sim_labels.view(-1, 1), value=1.0)
+    # # weighted score ---> [B, C]
+    # pred_scores = torch.sum(one_hot_label.view(feature.size(0), -1, classes) * sim_weight.unsqueeze(dim=-1), dim=1)
+    #
+    # pred_labels = pred_scores.argsort(dim=-1, descending=True)
+    return sim_indices
 
 
 def main():
@@ -63,17 +104,6 @@ def main():
 
 
 
-    # # Criterion
-    # print(colored('Retrieve criterion', 'green'))
-    # criterion = get_criterion(p)
-    # print('Criterion is {}'.format(criterion.__class__.__name__))
-    # criterion = criterion.to(device)
-    #
-    # # Optimizer and scheduler
-    # print(colored('Retrieve optimizer', 'green'))
-    # optimizer = get_optimizer(p, model)
-    # print(optimizer)
-
     # Checkpoint
     if os.path.exists(p['pretext_checkpoint']):
         print(colored('Restart from checkpoint {}'.format(p['pretext_checkpoint']), 'green'))
@@ -94,14 +124,27 @@ def main():
         model.eval()
         total_top1, total_top5, total_num, feature_bank = 0.0, 0.0, 0, []
 
-        for batch in train_dataloader:
+        progress_bar = tqdm(train_dataloader)
+        for batch in progress_bar:
             images = batch['image'].to(device, non_blocking=True)
-            target = batch['target'].to(device, non_blocking=True)
+            # target = batch['target'].to(device, non_blocking=True)
 
             output = model(images)
             feature = F.normalize(output, dim=1)
             feature_bank.append(feature)
 
+
+        for batch in progress_bar:
+
+            images = batch['image'].to(device, non_blocking=True)
+            # target = batch['target'].to(device, non_blocking=True)
+
+            output = model(images)
+            feature = F.normalize(output, dim=1)
+
+            sim_indices = knn_predict(feature, feature_bank, "", "", 10, 0.1)
+
+            print(sim_indices)
 
 if __name__ == '__main__':
     main()
