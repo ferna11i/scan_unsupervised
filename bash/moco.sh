36bc72c058121d25a86568267066d0acdda199ca
Johan <johan.27011993@gmail.com>
2021-03-15 01:17:38 -0400

    loader issues

--- bash/moco.sh
+++ bash/moco.sh
@@ -7,8 +7,8 @@
 #SBATCH -A soscip-3-040
 #SBATCH --mail-type=FAIL
 
-module load anaconda3 cuda cudnn gcc/8.3.0
-source activate detectionEnv
+module load anaconda3
+source activate clusterEnv
 
 python ../moco_upd.py --config_env ../configs/env.yml --config_exp ../configs/pretext/moco_tabledb.yml
 
