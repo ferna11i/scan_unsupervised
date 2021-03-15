481aa35a107a383bb8b82770ea51ad7ac3027c3c
Johan <johan.27011993@gmail.com>
2021-03-15 03:28:46 -0400

    faiss issues

--- moco_upd.py
+++ moco_upd.py
@@ -109,9 +109,9 @@ def main():
  
     # Save final model
     print(colored('Save pretext model', 'green'))
-    torch.save(model.state_dict(), p['pretext_model'])
+    torch.save(model.module.state_dict(), p['pretext_model'])
     # model.contrastive_head = torch.nn.Identity() # In this case, we mine the neighbors before the MLP.
-    model.contrastive_head = Identity()
+    model.module.contrastive_head = Identity()
     
     # Mine the topk nearest neighbors (Train)
     # These will be used for training with the SCAN-Loss.
